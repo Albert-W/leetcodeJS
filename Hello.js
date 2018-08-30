b0ee929088f1c81f5bd5f36a93a17fe19ad26c4e
Albert-W <wuyichang1990@gmail.com>
2018-08-30 17:20:18 +0100

    JavaScript内置对象

--- Hello.js
+++ Hello.js
@@ -5,15 +5,26 @@ var b = ++a;
 var c = b++;
 
 
-console.log(s + "\n");
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(s + "\n");
+// console.log(a);
+// console.log(b);
+// console.log(c);
+//
+// var my = new Array(3);
+// my[0] = 1;
+// my[1] = 1;
+// my[2] = 1;
+// my[4] = 10;
+// console.log(my[4]); // 10
+// console.log(my[3]); //undefined
 
-var my = new Array(3);
-my[0] = 1;
-my[1] = 1;
-my[2] = 1;
-my[4] = 10;
-console.log(my[4]); // 10
-console.log(my[3]); //undefined
+var d = new Date();
+console.log(d.getDay()); //2018-08-30T11:44:09.232Z
+console.log(d.getDay());//4
+
+console.log(d.getDate());//30
+console.log(d.getFullYear());//2018;
+console.log(d.getMonth()); //需要+1；
+var n = s.indexOf("o", 4);
+
+console.log(n);
