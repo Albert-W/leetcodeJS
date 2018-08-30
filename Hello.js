05941fade1414a5a264d9057ec9022e2c27231d6
Albert-W <wuyichang1990@gmail.com>
2018-08-30 11:57:20 +0100

    JavaScript进阶篇

--- Hello.js
+++ Hello.js
@@ -1,3 +1,19 @@
 var s = "hello";
 s += " world!";
-console.log(s);
+var a = 10;
+var b = ++a;
+var c = b++;
+
+
+console.log(s + "\n");
+console.log(a);
+console.log(b);
+console.log(c);
+
+var my = new Array(3);
+my[0] = 1;
+my[1] = 1;
+my[2] = 1;
+my[4] = 10;
+console.log(my[4]); // 10
+console.log(my[3]); //undefined
